52101e941321157e7a090a2f84e424dd4d9dab13
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-06-01 20:35:52 +0200

    Add files via upload

--- Swing/Fuentes/FuentesInstaladas.java
+++ Swing/Fuentes/FuentesInstaladas.java
@@ -12,3 +12,266 @@ public class FuentesInstaladas {
         }
     }
 }
+/*
+C:\Users\34660\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.2\lib\idea_rt.jar=50968:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.2\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\34660\IdeaProjects\PildorasInformaticasJava\out\production\PildorasInformaticasJava Swing.Fuentes.FuentesInstaladas
+Agency FB
+Algerian
+Arial
+Arial Black
+Arial Narrow
+Arial Nova
+Arial Nova Cond
+Arial Nova Cond Light
+Arial Nova Light
+Arial Rounded MT Bold
+Bahnschrift
+Baskerville Old Face
+Bauhaus 93
+Bell MT
+Berlin Sans FB
+Berlin Sans FB Demi
+Bernard MT Condensed
+Blackadder ITC
+Bodoni MT
+Bodoni MT Black
+Bodoni MT Condensed
+Bodoni MT Poster Compressed
+Book Antiqua
+Bookman Old Style
+Bookshelf Symbol 7
+Bradley Hand ITC
+Britannic Bold
+Broadway
+Brush Script MT
+Calibri
+Calibri Light
+Californian FB
+Calisto MT
+Cambria
+Cambria Math
+Candara
+Candara Light
+Castellar
+Centaur
+Century
+Century Gothic
+Century Schoolbook
+Chiller
+Colonna MT
+Comic Sans MS
+Consolas
+Constantia
+Cooper Black
+Copperplate Gothic Bold
+Copperplate Gothic Light
+Corbel
+Corbel Light
+Courier New
+Curlz MT
+Dialog
+DialogInput
+Dubai
+Dubai Light
+Dubai Medium
+Ebrima
+Edwardian Script ITC
+Elephant
+Engravers MT
+Eras Bold ITC
+Eras Demi ITC
+Eras Light ITC
+Eras Medium ITC
+Felix Titling
+Footlight MT Light
+Forte
+Franklin Gothic Book
+Franklin Gothic Demi
+Franklin Gothic Demi Cond
+Franklin Gothic Heavy
+Franklin Gothic Medium
+Franklin Gothic Medium Cond
+Freestyle Script
+French Script MT
+Gabriola
+Gadugi
+Garamond
+Georgia
+Georgia Pro
+Georgia Pro Black
+Georgia Pro Cond
+Georgia Pro Cond Black
+Georgia Pro Cond Light
+Georgia Pro Cond Semibold
+Georgia Pro Light
+Georgia Pro Semibold
+Gigi
+Gill Sans MT
+Gill Sans MT Condensed
+Gill Sans MT Ext Condensed Bold
+Gill Sans Nova
+Gill Sans Nova Cond
+Gill Sans Nova Cond Lt
+Gill Sans Nova Cond Ultra Bold
+Gill Sans Nova Cond XBd
+Gill Sans Nova Light
+Gill Sans Nova Ultra Bold
+Gill Sans Ultra Bold
+Gill Sans Ultra Bold Condensed
+Gloucester MT Extra Condensed
+Goudy Old Style
+Goudy Stout
+Haettenschweiler
+Harlow Solid Italic
+Harrington
+High Tower Text
+HoloLens MDL2 Assets
+HP Simplified
+HP Simplified Hans
+HP Simplified Hans Light
+HP Simplified Jpan
+HP Simplified Jpan Light
+HP Simplified Light
+Impact
+Imprint MT Shadow
+Informal Roman
+Ink Free
+Javanese Text
+Jokerman
+Juice ITC
+Kristen ITC
+Kunstler Script
+Leelawadee UI
+Leelawadee UI Semilight
+Lucida Bright
+Lucida Calligraphy
+Lucida Console
+Lucida Fax
+Lucida Handwriting
+Lucida Sans
+Lucida Sans Typewriter
+Lucida Sans Unicode
+Magneto
+Maiandra GD
+Malgun Gothic
+Malgun Gothic Semilight
+Marlett
+Matura MT Script Capitals
+Microsoft Himalaya
+Microsoft JhengHei
+Microsoft JhengHei Light
+Microsoft JhengHei UI
+Microsoft JhengHei UI Light
+Microsoft New Tai Lue
+Microsoft PhagsPa
+Microsoft Sans Serif
+Microsoft Tai Le
+Microsoft YaHei
+Microsoft YaHei Light
+Microsoft YaHei UI
+Microsoft YaHei UI Light
+Microsoft Yi Baiti
+MingLiU-ExtB
+MingLiU_HKSCS-ExtB
+Mistral
+Modern No. 20
+Mongolian Baiti
+Monospaced
+Monotype Corsiva
+MS Gothic
+MS Outlook
+MS PGothic
+MS Reference Sans Serif
+MS Reference Specialty
+MS UI Gothic
+MT Extra
+MV Boli
+Myanmar Text
+Neue Haas Grotesk Text Pro
+Niagara Engraved
+Niagara Solid
+Nirmala UI
+Nirmala UI Semilight
+NSimSun
+OCR A Extended
+Old English Text MT
+Onyx
+Palace Script MT
+Palatino Linotype
+Papyrus
+Parchment
+Perpetua
+Perpetua Titling MT
+Playbill
+PMingLiU-ExtB
+Poor Richard
+Pristina
+Rage Italic
+Ravie
+Rockwell
+Rockwell Condensed
+Rockwell Extra Bold
+Rockwell Nova
+Rockwell Nova Cond
+Rockwell Nova Cond Light
+Rockwell Nova Extra Bold
+Rockwell Nova Light
+Sans Serif Collection
+SansSerif
+Script MT Bold
+Segoe Fluent Icons
+Segoe MDL2 Assets
+Segoe Print
+Segoe Script
+Segoe UI
+Segoe UI Black
+Segoe UI Emoji
+Segoe UI Historic
+Segoe UI Light
+Segoe UI Semibold
+Segoe UI Semilight
+Segoe UI Symbol
+Segoe UI Variable
+Serif
+Showcard Gothic
+SimSun
+SimSun-ExtB
+Sitka Text
+Snap ITC
+Stencil
+Sylfaen
+Symbol
+Tahoma
+Tempus Sans ITC
+Times New Roman
+Trebuchet MS
+Tw Cen MT
+Tw Cen MT Condensed
+Tw Cen MT Condensed Extra Bold
+Verdana
+Verdana Pro
+Verdana Pro Black
+Verdana Pro Cond
+Verdana Pro Cond Black
+Verdana Pro Cond Light
+Verdana Pro Cond Semibold
+Verdana Pro Light
+Verdana Pro Semibold
+Viner Hand ITC
+Vivaldi
+Vladimir Script
+Webdings
+Wide Latin
+Wingdings
+Wingdings 2
+Wingdings 3
+Yu Gothic
+Yu Gothic Light
+Yu Gothic Medium
+Yu Gothic UI
+Yu Gothic UI Light
+Yu Gothic UI Semibold
+Yu Gothic UI Semilight
+
+Process finished with exit code 0
+
+ */
