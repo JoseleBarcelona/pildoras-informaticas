a242759ad9e8407cdf904bbb32cddb1d3e7dee7a
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-10-09 23:09:36 +0200

    Add files via upload

--- Python/archivosExternos/writeReadSeek.py
+++ Python/archivosExternos/writeReadSeek.py
@@ -6,50 +6,60 @@ from io import open
 # frase="Hola programadores\nbienvenidos al curso de Python."
 # archivo_texto.write(frase)
 # archivo_texto.close()
+# del (archivo_texto) #Borramos la variable para que no se quede en el disco duro
 
 # archivo_texto=open("indext.txt", "r")
 # texto=archivo_texto.read()
 # archivo_texto.close()
 # print(texto)
+# del (archivo_texto)
 
 # archivo_texto=open("indext.txt", "r")
 # lineas_texto=archivo_texto.readlines()
 # archivo_texto.close()
 # print(lineas_texto[0])
+# del (archivo_texto)
 
 # archivo_texto=open("indext.txt", "a")
 # archivo_texto.write("\nSiempre es un placer aprender Python")
 # archivo_texto.close()
+# del (archivo_texto)
 
 # archivo_texto=open("indext.txt", "r")
 # print(archivo_texto.read())
 # archivo_texto.seek(0)
 # print(archivo_texto.read())
 # archivo_texto.close()
+# del (archivo_texto)
 
 # archivo_texto=open("indext.txt", "r")
 # archivo_texto.seek(11)
 # print(archivo_texto.read())
 # archivo_texto.close()
+# del (archivo_texto)
 
 # archivo_texto=open("indext.txt", "r")
 # print(archivo_texto.read(11))
 # print(archivo_texto.read())
 # archivo_texto.close()
+# del (archivo_texto)
 
 # archivo_texto=open("indext.txt", "r")
 # archivo_texto.seek(len(archivo_texto.read())/2)
 # print(archivo_texto.read())
 # archivo_texto.close()
+# del (archivo_texto)
 
 # archivo_texto=open("indext.txt", "r")
 # archivo_texto.seek(len(archivo_texto.readline()))
 # print(archivo_texto.read())
 # archivo_texto.close()
+# del (archivo_texto)
 
 # archivo_texto=open("indext.txt", "r+")
 # archivo_texto.write("Cambio de texto inicial")
 # archivo_texto.close()
+# del (archivo_texto)
 
 # archivo_texto=open("indext.txt", "r+")
 # lista_texto=archivo_texto.readlines()
@@ -57,3 +67,4 @@ from io import open
 # archivo_texto.seek(0)
 # archivo_texto.writelines(lista_texto)
 # archivo_texto.close()
+# del (archivo_texto)
