7072294cb9fb10605b3bd754fbe439fb2e44099e
Josele Barcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-12-06 21:09:05 +0100

    Add files via upload
    Crear superusuario en el panel de administración de Django

--- Django/tiendaOnline/App_gestionPedidos/models.py
+++ Django/tiendaOnline/App_gestionPedidos/models.py
@@ -13,8 +13,10 @@ class Articulos(models.Model):
     seccion=models.CharField(max_length=20)
     precio=models.IntegerField()
 
+    def __str__(self):
+        return 'El nombre del artículo es %s, la sección es %s y el precio es %s' % (self.nombre, self.seccion, self.precio) 
+
 class Pedidos(models.Model):
     numero=models.IntegerField()
     fecha=models.DateField()
     entregado=models.BooleanField()
-
