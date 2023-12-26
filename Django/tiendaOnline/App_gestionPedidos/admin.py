6ff24c2e5638c4c2fbd147c0440e27c8dc996e84
Josele Barcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-12-26 23:43:05 +0100

    Agregar clases a admin.py

--- Django/tiendaOnline/App_gestionPedidos/admin.py
+++ Django/tiendaOnline/App_gestionPedidos/admin.py
@@ -3,6 +3,18 @@ from App_gestionPedidos.models import Clientes, Articulos, Pedidos
 
 # Register your models here.
 
-admin.site.register(Clientes)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=('nombre', 'direccion', 'telefono') #muestra en el panel de admin los campos que queremos
+    search_fields=('nombre', 'telefono') #barra de búsqueda
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter=('seccion','nombre',) #Crea una interfaz que filtra por sección y por nombre(Ponemos una , al final ya que es una tupla)
+    
+class PedidosAdmin(admin.ModelAdmin):
+    list_filter=('fecha',)
+    list_display=('numero', 'fecha', 'entregado', 'id') 
+    date_hierarchy='fecha' #crea en la parte superior de admin un historial de fechas
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
--- Django/tiendaOnline/App_gestionPedidos/models.py
+++ Django/tiendaOnline/App_gestionPedidos/models.py
@@ -4,9 +4,12 @@ from django.db import models
 
 class Clientes(models.Model):
     nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=50, verbose_name='dirección Empresa')
     email=models.EmailField()
     telefono=models.CharField(max_length=9)
+
+    def __str__(self):
+        return self.nombre #el método __str__() devuelve un string y no un objeto en el panel admin
 
 class Articulos(models.Model):
     nombre=models.CharField(max_length=30)
@@ -14,7 +17,7 @@ class Articulos(models.Model):
     precio=models.IntegerField()
 
     def __str__(self):
-        return 'El nombre del artículo es %s, la sección es %s y el precio es %s' % (self.nombre, self.seccion, self.precio) 
+        return 'Nombre: %s, Sección: %s y Precio %s' % (self.nombre, self.seccion, self.precio) 
 
 class Pedidos(models.Model):
     numero=models.IntegerField()
